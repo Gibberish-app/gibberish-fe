9bc16477ffae5a8096949b00260d5c5f612f62df
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-03-26 14:16:43 -0700

    boardMap values defined

---
+++ src/components/cells/boardMap.js
@@ -0,0 +1,26 @@
+const boardMap = {
+    0: {
+        'value': '',
+        'color': '#0A2239'
+    },
+    1: {
+        'value': 'Double Letter',
+        'color': '#84BCDA'
+    },
+    2: {
+        'value': 'Triple Letter',
+        'color': '#067BC2'
+    },
+    3: {
+        'value': 'Double Word',
+        'color': '#F37748'
+    },
+    4: {
+        'value': 'Triple Word',
+        'color': '#D56062',
+    },
+    5: {
+        'value': '&#9733;',
+        'color': '#F37748'
+    }
+}
