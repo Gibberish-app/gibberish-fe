b3b6ae5de2b7965480b00948b13954e5be0d6ca0
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-27 16:24:37 -0700

    players can create & join games

--- src/containers/GameWindow.jsx
+++ src/containers/GameWindow.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useRef } from 'react'
 import LandingPage from './LandingPage/LandingPage';
 import Board from '../components/board/Board';
 import Player from '../components/player/Player';
@@ -8,7 +8,8 @@ import Header from '../components/header/Header';
 const GameWindow = () => {
     const [active, setActive] = useState(false);
     const [currentUser, setCurrentUser] = useState({})
-    const [bag, setBag] = useState(0)
+    const [currentGame, setCurrentGame] = useState({})
+
 
     const handleActive = () => {
         setActive(true);
@@ -17,19 +18,21 @@ const GameWindow = () => {
     return (
         <div>
             {active ?
-            <div>
-                <Header />
-                <Board />
-                <GameInfo 
+                <div>
+                    <Header />
+                    <Board />
+                    <GameInfo
+                        currentUser={currentUser}
+                        currentGame={currentGame} />
+                    <Player setBag={setBag} />
+                </div>
+                :
+                <LandingPage
+                    handleActive={handleActive}
+                    setCurrentUser={setCurrentUser}
+                    setCurrentGame={setCurrentGame}
                     currentUser={currentUser}
-                    bag={bag} />
-                <Player setBag={setBag}/>
-            </div>
-            :
-            <LandingPage
-                handleActive={handleActive}
-                setCurrentUser={setCurrentUser}
-            />
+                />
             }
         </div>
     )
--- src/utils/user/gameList.js
+++ src/utils/user/gameList.js
@@ -1,7 +1,6 @@
 import socket from '../socket/socket'
 
 export default function gameList() {
-    socket.emit("GAME_LIST");
 
     socket.on("GAME_LIST", openGames => {
         return openGames;
