98a42e2c8899b5a6c1c9a8c888209e642e2cd892
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-03-28 10:24:35 -0700

    Waiting centered

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -22,7 +22,6 @@ export default function Player({ currentGame, addTile, currentHand, seedHand, ha
         addTile(tile, index);
     }
 
-
     useEffect(() => {
         console.log('waiting changed')
         if (waiting === false) {
