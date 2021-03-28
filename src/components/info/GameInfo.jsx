9178c476064a89a2626bd2ad2ef90da0c46f8faf
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-03-27 18:12:34 -0700

    Both users rendering in GameWindow

--- src/components/info/GameInfo.jsx
+++ src/components/info/GameInfo.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './gameinfo.css';
 
-const GameInfo = ({ currentUser, bag }) => {
+const GameInfo = ({ currentUser, bag, currentGame }) => {
     return (
         <div className={styles.info}>
             <img src={`/avatars/${currentUser.avatar}.png`} />
             <span>{currentUser.userName}</span>
             <p>Score: </p>
+            <p>VS.</p>
+            <img src={`/avatars/${currentGame.users[0].avatar}.png`} />
+            <span>{currentGame.users[0].userName}</span>
+            <p>Score: </p>
+            <br />
+            <br />
             <p>Tiles Remaining: {bag}</p>
         </div>
     )
